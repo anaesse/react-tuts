f7eee01c1be12d687e7f02525f1b27fb124c39f4
Sophia Edeki <sophiesse143@gmail.com>
2022-05-22 23:53:16 +0100

    updated

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-ReactDOM.render(<h1>Hello, i'm learning react</h1>, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello, i'm learning react</h1>, document.getElementById("root"));
+// ReactDOM.render(<ul>
+//     <li>hello</li>
+//     <li>hi</li>
+//     <li>hey</li>
+// </ul>, document.getElementById("root"));
+
+// const navbar = (
+//     <nav>
+//     <h1>NyleZeeds</h1>
+//     <ul>
+//         <li>Pricing</li>
+//         <li>About</li>
+//         <li>Contact</li>
+//     </ul>
+// </nav>
+
+// )
+    
+// ReactDOM.render( navbar, document.getElementById("root"));
+
+const page = (
+    <div>
+        <h1>hello, i'm sophia</h1>
+        <h4>why i love react</h4>
+        <ul>
+            <li> it's declarative</li>
+            <li> it's hireable</li>
+        </ul>
+    </div>
+)
+ReactDOM.render(page, document.getElementById("root"))
